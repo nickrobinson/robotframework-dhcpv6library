2604a11e8ea933d62651bf84b422811ec276a6ef
Nick Robinson <nrobinson13@gmail.com>
2014-01-16 10:01:34 -0600

    Updating DHCPv6Library as previous version did not support consistent DUID's due to error in use of RandMAC function

--- DHCPv6Library.py
+++ DHCPv6Library.py
@@ -1,27 +1,25 @@
 #! /usr/bin/env python
 
-from scapy import *
+from scapy.all import *
 import random
 
 class DHCPv6Library:
+	#globalMac = RandMAC()
 
 	def __init__(self, interface):
 		self.__interface = interface
-		self.mac = RandMAC()
        		self.transactionId = random.randint(0, 1677215)
+		self.mac = RandMAC()._fix()
  
-	def send_solicit(self, macAddr="", type="IAPD", t1=0, t2=0, srcIp ="fe80::20e:7bff:febb:a38a"):
-		if macAddr:
-			self.mac = macAddr
+	def send_solicit(self, type="IAPD", t1=0, t2=0, srcIp ="fe80::20e:7bff:febb:a38a"):
+		self.clientid = DHCP6OptClientId()
+		self.clientid.duid = DUID_LL(lladdr=self.mac)
 		self.ether = Ether(dst="33:33:00:01:00:02", src=self.mac)
 		self.ipv6 = IPv6(src=srcIp, dst="ff02::1:2")
 		self.udp = UDP(sport=546, dport=547)
-		self.clientid = DHCP6OptClientId()
-		self.clientid.duid = DUID_LL(lladdr=self.mac)
 		self.iapd = DHCP6OptIA_PD(iaid=4, T1=150000, T2=200000)
 		self.elapsedTime = DHCP6OptElapsedTime()
 		solicit = DHCP6_Solicit(trid=self.transactionId)
-
 		solicitPacket = (self.ether/self.ipv6/self.udp/solicit/self.clientid/self.iapd/self.elapsedTime)
  		self.ans,unans=srp(solicitPacket, iface=self.__interface)
 
@@ -35,10 +33,9 @@ class DHCPv6Library:
 		packet = (self.ether/self.ipv6/self.udp/request/self.clientid/serverId/self.elapsedTime)
                 sendp(packet, iface=self.__interface)
 
-	def get_mac(self):
-		return self.mac
-
 	def get_returned_prefix(self):
 		prefix = self.ans[0][1][DHCP6OptIAPrefix].prefix
 		return prefix
-			
+
+	def get_mac(self):
+		return self.mac
